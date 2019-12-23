ac618585db824e5e61b5221215329c04518b84c0
travis Lawson <travislawclimb@gmail.com>
2019-12-23 07:47:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,24 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
-
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-roll_call_dwarves(dwarves)
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
+end
+
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end
+end
+
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
+end
